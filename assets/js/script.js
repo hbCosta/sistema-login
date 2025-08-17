9564eadd84f06b79fe338d601dd404081952d3bc
Humberto Costa <bandeiracosta67@gmail.com>
2025-08-17 12:16:42 -0300

    Refatoração do sistema de login: reorganização dos estilos CSS, adição de validação no formulário de login e criação da página de login. Remoção da página de registro.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('form');
+    if(form){
+        form.addEventListener('submit', function(e){
+            e.preventDefault();
+            // Validar campos
+            const email = document.getElementById('email');
+            const password = document.getElementById('password');
+
+            if(email.value === '' || password.value === ''){
+                alert('Por favor, preencha todos os campos');
+                return;
+            }
+            SubmitForm(email,password);
+        });
+    }   
+});
